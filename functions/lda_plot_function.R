b185045bdf8fdad6635148c48e9d7a1a0692e64e
Erica Christensen <erica.christensen@weecology.org>
2018-07-18 13:18:19 -0400

    added function to plot community timeseries to lda_plot_function.R

--- functions/lda_plot_function.R
+++ functions/lda_plot_function.R
@@ -64,3 +64,58 @@ community_composition = function(ldamodel) {
   return(structure(round(exp(ldamodel@beta), 3), dimnames = list(NULL, ldamodel@terms)))
 }
 
+#' Plot gamma
+#' 
+#' @description This plotting function plots the component communities over time
+#' It's used by the plot_component_communities function, but can also take
+#' any data frame as input as long as it's the right form -- used by the simulations
+#' 
+#' @param gamma_frame a data frame containing columns for date, relabund, and community
+#' @param ntopics number of topics
+#' @param ylab label for y axis (optional)
+#' 
+#' @return a ggplot object
+plot_gamma = function(gamma_frame,ntopics,ylab='',colors=cbPalette) {
+  g = ggplot2::ggplot(gamma_frame, aes(x=date,y=relabund,colour=community)) + 
+    geom_line(size=1) +
+    scale_y_continuous(name=ylab,limits=c(0,1)) +
+    scale_x_date(name='') +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=12),
+          panel.background = element_rect(colour='white',fill='white'),
+          panel.grid.major = element_line(colour = "gray90"),
+          panel.grid.minor = element_line(colour = 'gray90'),
+          panel.border=element_rect(colour='black',fill=NA),
+          legend.position='none') +
+    scale_colour_manual(name="Component\nCommunity",
+                        breaks=as.character(seq(ntopics)),
+                        values=colors[1:ntopics],
+                        guide=FALSE)
+  return(g)
+}
+
+
+#' Plot component communities
+#' 
+#' @description Plots timeseries of component communities (topics)
+#' 
+#' @param ldamodel object of class LDA_VEM created by the function LDA in topicmodels package
+#' @param ntopics number of topics used in ldamodel
+#' @param xticks vector of dates for x-axis labels
+#' @param ylab y axis label (optional)
+#' @param topic_order order of topics (for color control)
+#' 
+#' @return ggplot object
+#' 
+#' @example plot_component_communities(ldamodel,ntopics,period_dates$date)
+plot_component_communities = function(ldamodel,ntopics,xticks,ylab='',topic_order = seq(ntopics)) {
+  cbPalette <- c( "#e19c02","#999999", "#56B4E9", "#0072B2", "#D55E00", "#F0E442", "#009E73", "#CC79A7")
+  z = posterior(ldamodel)
+  ldaplot = data.frame(date = c(), relabund = c(), community = c())
+  for (t in topic_order) {
+    ldaplot = rbind(ldaplot, data.frame(date=xticks ,relabund=z$topics[,t], community = as.factor(rep(t,length(z$topics[,1])))))
+  }
+  g = plot_gamma(ldaplot, ntopics, ylab, colors=cbPalette)
+  return(g) 
+}
+
